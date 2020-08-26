5f6e1540d34ce4f5f91d91a6c1cec9a13415aac1
Tal Gendler <tal.gendler@gmail.com>
2020-08-26 17:09:39 +0300

    introduce new type providers

---
+++ providers/types/http.go
@@ -0,0 +1,13 @@
+package types
+
+import "github.com/go-masonry/mortar/constructors/partial"
+
+// HTTPHandlerPatternPair defines pattern -> handler pair
+//
+// Use this type to provide new Internal HTTP Handler
+type HTTPHandlerPatternPair = partial.HTTPHandlerPatternPair
+
+// HTTPHandlerFuncPatternPair defines patter -> handler func pair
+//
+// Use this type to provide new Internal HTTP Handler
+type HTTPHandlerFuncPatternPair = partial.HTTPHandlerFuncPatternPair
