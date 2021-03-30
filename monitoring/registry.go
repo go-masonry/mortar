1c5cfe9282cef02d71960eedbd0f2bd93c5f59e1
Tal <talgendler@users.noreply.github.com>
2021-03-30 13:11:18 +0300

    fix metrics creation race condition (#26)
    * fix metrics creation race condition
    
    * fix metrics creation race condition avoid double check

--- monitoring/registry.go
+++ monitoring/registry.go
@@ -9,7 +9,10 @@ import (
 )
 
 type externalRegistry struct {
-	sync.Mutex
+	cm       sync.RWMutex
+	gm       sync.RWMutex
+	tm       sync.RWMutex
+	hm       sync.RWMutex
 	external monitor.BricksMetrics
 	// TODO perhaps change this to self evicting cache that will remove metrics if unused for a long time to save space
 	counters   *sync.Map
@@ -28,60 +31,84 @@ func newRegistry(externalMetrics monitor.BricksMetrics) *externalRegistry {
 	}
 }
 
-func (r *externalRegistry) loadOrStoreCounter(name, desc string, keys ...string) (monitor.BricksCounter, error) {
+func (r *externalRegistry) loadOrStoreCounter(name, desc string, keys ...string) (bricksCounter monitor.BricksCounter, err error) {
 	ID := calcID(name, keys...)
 	if known, ok := r.counters.Load(ID); ok {
 		return known.(monitor.BricksCounter), nil
 	}
-	r.Lock()
-	defer r.Unlock()
-	bricksCounter, err := r.external.Counter(name, desc, keys...)
+	r.cm.Lock()
+	defer r.cm.Unlock()
+	bricksCounter, err = r.external.Counter(name, desc, keys...)
 	if err == nil {
-		r.counters.LoadOrStore(ID, bricksCounter)
+		cacheValue, _ := r.counters.LoadOrStore(ID, bricksCounter) // if a previous duplicate is already there (was created by other go routine)
+		bricksCounter = cacheValue.(monitor.BricksCounter)
+	} else {
+		if known, ok := r.counters.Load(ID); ok { // perhaps it's already there (was created by other go routine) and the underlying impl have a dup check
+			bricksCounter, err = known.(monitor.BricksCounter), nil
+			return
+		}
 	}
-	return bricksCounter, err
+	return
 }
 
-func (r *externalRegistry) loadOrStoreGauge(name, desc string, keys ...string) (monitor.BricksGauge, error) {
+func (r *externalRegistry) loadOrStoreGauge(name, desc string, keys ...string) (bricksGauge monitor.BricksGauge, err error) {
 	ID := calcID(name, keys...)
 	if known, ok := r.gauges.Load(ID); ok {
 		return known.(monitor.BricksGauge), nil
 	}
-	r.Lock()
-	defer r.Unlock()
-	bricksGauge, err := r.external.Gauge(name, desc, keys...)
+	r.gm.Lock()
+	defer r.gm.Unlock()
+	bricksGauge, err = r.external.Gauge(name, desc, keys...)
 	if err == nil {
-		r.gauges.LoadOrStore(ID, bricksGauge)
+		cacheValue, _ := r.gauges.LoadOrStore(ID, bricksGauge) // if a previous duplicate is already there (was created by other go routine)
+		bricksGauge = cacheValue.(monitor.BricksGauge)
+	} else {
+		if known, ok := r.gauges.Load(ID); ok { // perhaps it's already there (was created by other go routine) and the underlying impl have a dup check
+			bricksGauge, err = known.(monitor.BricksGauge), nil
+			return
+		}
 	}
-	return bricksGauge, err
+	return
 }
 
-func (r *externalRegistry) loadOrStoreHistogram(name, desc string, buckets monitor.Buckets, keys ...string) (monitor.BricksHistogram, error) {
+func (r *externalRegistry) loadOrStoreHistogram(name, desc string, buckets monitor.Buckets, keys ...string) (bricksHistogram monitor.BricksHistogram, err error) {
 	ID := calcID(name, keys...)
 	if known, ok := r.histograms.Load(ID); ok {
 		return known.(monitor.BricksHistogram), nil
 	}
-	r.Lock()
-	defer r.Unlock()
-	bricksHistogram, err := r.external.Histogram(name, desc, buckets, keys...)
+	r.hm.Lock()
+	defer r.hm.Unlock()
+	bricksHistogram, err = r.external.Histogram(name, desc, buckets, keys...)
 	if err == nil {
-		r.histograms.LoadOrStore(ID, bricksHistogram)
+		cacheValue, _ := r.histograms.LoadOrStore(ID, bricksHistogram) // if a previous duplicate is already there (was created by other go routine)
+		bricksHistogram = cacheValue.(monitor.BricksHistogram)
+	} else {
+		if known, ok := r.histograms.Load(ID); ok { // perhaps it's already there (was created by other go routine) and the underlying impl have a dup check
+			bricksHistogram, err = known.(monitor.BricksHistogram), nil
+			return
+		}
 	}
-	return bricksHistogram, err
+	return
 }
 
-func (r *externalRegistry) loadOrStoreTimer(name, desc string, keys ...string) (monitor.BricksTimer, error) {
+func (r *externalRegistry) loadOrStoreTimer(name, desc string, keys ...string) (bricksTimer monitor.BricksTimer, err error) {
 	ID := calcID(name, keys...)
 	if known, ok := r.timers.Load(ID); ok {
 		return known.(monitor.BricksTimer), nil
 	}
-	r.Lock()
-	defer r.Unlock()
-	bricksTimer, err := r.external.Timer(name, desc, keys...)
+	r.tm.Lock()
+	defer r.tm.Unlock()
+	bricksTimer, err = r.external.Timer(name, desc, keys...)
 	if err == nil {
-		r.timers.LoadOrStore(ID, bricksTimer)
+		cacheValue, _ := r.timers.LoadOrStore(ID, bricksTimer) // if a previous duplicate is already there (was created by other go routine)
+		bricksTimer = cacheValue.(monitor.BricksTimer)
+	} else {
+		if known, ok := r.timers.Load(ID); ok { // perhaps it's already there (was created by other go routine) and the underlying impl have a dup check
+			bricksTimer, err = known.(monitor.BricksTimer), nil
+			return
+		}
 	}
-	return bricksTimer, err
+	return
 }
 
 func calcID(name string, keys ...string) (ID string) {
--- monitoring/registry_test.go
+++ monitoring/registry_test.go
@@ -1,11 +1,16 @@
 package monitoring
 
 import (
+	"fmt"
+	"strconv"
+	"sync"
 	"testing"
 
+	"github.com/go-masonry/mortar/interfaces/monitor"
 	mock_monitor "github.com/go-masonry/mortar/interfaces/monitor/mock"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRegistryCounterCache(t *testing.T) {
@@ -57,4 +62,187 @@ func TestIDGen(t *testing.T) {
 	assert.Equal(t, "name_first_second", ID)
 	sameID := calcID("name", "second", "first")
 	assert.Equal(t, ID, sameID)
+}
+
+func TestRegistryReturnsError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	externalMock := mock_monitor.NewMockBricksMetrics(ctrl)
+	registry := newRegistry(externalMock)
+	externalMock.EXPECT().Histogram(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, fmt.Errorf("constant error")).Times(2)
+	_, err1 := registry.loadOrStoreHistogram("name", "desc", nil, "one", "two")
+	_, err2 := registry.loadOrStoreHistogram("name", "desc", nil, "one", "two")
+	assert.EqualError(t, err1, "constant error")
+	assert.EqualError(t, err2, "constant error")
+	externalMock.EXPECT().Timer(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, fmt.Errorf("constant error")).Times(2)
+	_, err1 = registry.loadOrStoreTimer("name", "desc", "one", "two")
+	_, err2 = registry.loadOrStoreTimer("name", "desc", "one", "two")
+	assert.EqualError(t, err1, "constant error")
+	assert.EqualError(t, err2, "constant error")
+	externalMock.EXPECT().Counter(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, fmt.Errorf("constant error")).Times(2)
+	_, err1 = registry.loadOrStoreCounter("name", "desc", "one", "two")
+	_, err2 = registry.loadOrStoreCounter("name", "desc", "one", "two")
+	assert.EqualError(t, err1, "constant error")
+	assert.EqualError(t, err2, "constant error")
+	externalMock.EXPECT().Gauge(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, fmt.Errorf("constant error")).Times(2)
+	_, err1 = registry.loadOrStoreGauge("name", "desc", "one", "two")
+	_, err2 = registry.loadOrStoreGauge("name", "desc", "one", "two")
+	assert.EqualError(t, err1, "constant error")
+	assert.EqualError(t, err2, "constant error")
+}
+
+func TestConcurrentGaugeCreation(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	externalMock := mock_monitor.NewMockBricksMetrics(ctrl)
+	brickGaugeMock := mock_monitor.NewMockBricksGauge(ctrl)
+	registry := newRegistry(externalMock)
+	for i := 0; i < 10; i++ {
+		var created bool
+		var mu sync.Mutex
+		name := strconv.Itoa(i)
+		var wg sync.WaitGroup
+		var err1, err2 error
+		externalMock.EXPECT().Gauge(name, gomock.Any(), gomock.Any()).DoAndReturn(func(name string, _ string, _ ...string) (monitor.BricksGauge, error) {
+			mu.Lock()
+			defer mu.Unlock()
+			if created {
+				return nil, fmt.Errorf("already exists")
+			}
+			created = true
+			return brickGaugeMock, nil
+		}).MaxTimes(2).MinTimes(1)
+		wg.Add(2)
+		// concurrent run 1
+		go func(n string) {
+			defer wg.Done()
+			_, err1 = registry.loadOrStoreGauge(n, "")
+		}(name)
+
+		// concurrent run 2
+		go func(n string) {
+			defer wg.Done()
+			_, err2 = registry.loadOrStoreGauge(n, "")
+		}(name)
+		wg.Wait()
+		require.NoError(t, err1, "[%d] first error", i)
+		require.NoError(t, err2, "[%d] second error", i)
+	}
+}
+
+func TestConcurrentTimerCreation(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	externalMock := mock_monitor.NewMockBricksMetrics(ctrl)
+	brickTimerMock := mock_monitor.NewMockBricksTimer(ctrl)
+	registry := newRegistry(externalMock)
+	for i := 0; i < 10; i++ {
+		var created bool
+		var mu sync.Mutex
+		name := strconv.Itoa(i)
+		var wg sync.WaitGroup
+		var err1, err2 error
+		externalMock.EXPECT().Timer(name, gomock.Any(), gomock.Any()).DoAndReturn(func(name string, _ string, _ ...string) (monitor.BricksTimer, error) {
+			mu.Lock()
+			defer mu.Unlock()
+			if created {
+				return nil, fmt.Errorf("already exists: %s", name)
+			}
+			created = true
+			return brickTimerMock, nil
+		}).MaxTimes(2).MinTimes(1)
+		wg.Add(2)
+		// concurrent run 1
+		go func(n string) {
+			defer wg.Done()
+			_, err1 = registry.loadOrStoreTimer(n, "")
+		}(name)
+
+		// concurrent run 2
+		go func(n string) {
+			defer wg.Done()
+			_, err2 = registry.loadOrStoreTimer(n, "")
+		}(name)
+		wg.Wait()
+		require.NoError(t, err1, "[%d] first error", i)
+		require.NoError(t, err2, "[%d] second error", i)
+	}
+}
+
+func TestConcurrentCounterCreation(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	externalMock := mock_monitor.NewMockBricksMetrics(ctrl)
+	brickCounterMock := mock_monitor.NewMockBricksCounter(ctrl)
+	registry := newRegistry(externalMock)
+	for i := 0; i < 10; i++ {
+		var created bool
+		var mu sync.Mutex
+		name := strconv.Itoa(i)
+		var wg sync.WaitGroup
+		var err1, err2 error
+		externalMock.EXPECT().Counter(name, gomock.Any(), gomock.Any()).DoAndReturn(func(name string, _ string, _ ...string) (monitor.BricksCounter, error) {
+			mu.Lock()
+			defer mu.Unlock()
+			if created {
+				return nil, fmt.Errorf("already exists")
+			}
+			created = true
+			return brickCounterMock, nil
+		}).MaxTimes(2).MinTimes(1)
+		wg.Add(2)
+		// concurrent run 1
+		go func(n string) {
+			defer wg.Done()
+			_, err1 = registry.loadOrStoreCounter(n, "")
+		}(name)
+
+		// concurrent run 2
+		go func(n string) {
+			defer wg.Done()
+			_, err2 = registry.loadOrStoreCounter(n, "")
+		}(name)
+		wg.Wait()
+		require.NoError(t, err1, "[%d] first error", i)
+		require.NoError(t, err2, "[%d] second error", i)
+	}
+}
+
+func TestConcurrentHistogramCreation(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	externalMock := mock_monitor.NewMockBricksMetrics(ctrl)
+	brickHistogramMock := mock_monitor.NewMockBricksHistogram(ctrl)
+	registry := newRegistry(externalMock)
+	for i := 0; i < 10; i++ {
+		var created bool
+		var mu sync.Mutex
+		name := strconv.Itoa(i)
+		var wg sync.WaitGroup
+		var err1, err2 error
+		externalMock.EXPECT().Histogram(name, gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(func(name string, _ string, _ []float64, _ ...string) (monitor.BricksHistogram, error) {
+			mu.Lock()
+			defer mu.Unlock()
+			if created {
+				return nil, fmt.Errorf("already exists")
+			}
+			created = true
+			return brickHistogramMock, nil
+		}).MaxTimes(2).MinTimes(1)
+		wg.Add(2)
+		// concurrent run 1
+		go func(n string) {
+			defer wg.Done()
+			_, err1 = registry.loadOrStoreHistogram(n, "", nil)
+		}(name)
+
+		// concurrent run 2
+		go func(n string) {
+			defer wg.Done()
+			_, err2 = registry.loadOrStoreHistogram(n, "", nil)
+		}(name)
+		wg.Wait()
+		require.NoError(t, err1, "[%d] first error", i)
+		require.NoError(t, err2, "[%d] second error", i)
+	}
 }
